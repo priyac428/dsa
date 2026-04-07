748255466514fb77f47d58714a8f777f18657a43
PRIYA <priyahin100@gmail.com>
2026-04-07 23:27:36 +0530

    Add updated

--- basic_math/divisors.py
+++ basic_math/divisors.py
@@ -4,24 +4,24 @@
 # Time Complexity: O(N)
 # Space Complexity: O(N)
 def divisors(n):
-    list0 = []
+    list1 = []
     for i in range(1,n+1):
         if n % i == 0:
-            list0.append(i)
-    return list0
+            list1.append(i)
+    return list1
 print(divisors(10))
 
 # Optimal Approach
 # Time Complexity: O(sqrt(N))
 # Space Complexity: O(sqrt(N))   
 def divisors1(n1):
-    list1 = []
+    list2 = []
     for i in range(1,int(n1**0.5)+1):
         if n1 % i == 0:
-            list1.append(i)
+            list2.append(i)
             if n1//i != i:
-                list1.append(n1//i)
-    list1.sort()
-    return list1
+                list2.append(n1//i)
+    list2.sort()
+    return list2
 print(divisors1(20)) 
 
