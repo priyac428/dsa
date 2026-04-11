79b4d5850e882bfe2c7c80674a3387cf8e06f431
PRIYA <priyahin100@gmail.com>
2026-04-11 23:04:17 +0530

    Add sum of n numbers

--- basic_recursion/sum_of_n.py
+++ basic_recursion/sum_of_n.py
@@ -1,10 +1,11 @@
 # Topic : Sum Of N Numbers
 # Time Complexity: O(N)
 # Space Complexity: O(N)
-def NnumbersSum(N):
-    if N == 0:
+
+def NnumbersSum(n):
+    if n == 0:
         return 0
-    return N + NnumbersSum(N - 1)
+    return n + NnumbersSum(n - 1)
 print(NnumbersSum(10))
 
         
