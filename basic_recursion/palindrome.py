b9fcb6ec3d2ec0624c65db7b96a4de0b85c10b93
PRIYA <priyahin100@gmail.com>
2026-04-13 00:20:33 +0530

    Add palindrome

---
+++ basic_recursion/palindrome.py
@@ -0,0 +1,13 @@
+  
+def palindromeCheck(s):
+    left = 0
+    right = len(s) - 1
+        
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+        
+    return True
+print(palindromeCheck("madam"))
