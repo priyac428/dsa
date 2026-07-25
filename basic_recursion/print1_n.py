12d059898eb988870ace7aca931423e67ffacf12
PRIYA <priyahin100@gmail.com>
2026-07-25 20:15:41 +0530

    updated

--- basic_recursion/print1_n.py
+++ basic_recursion/print1_n.py
@@ -5,9 +5,10 @@
 def printNumbers(n):        
     if n == 0:
         return
-    printNumbers(n-1)
-    print(n,end="")
-    print("")
+    else:
+        printNumbers(n-1)
+        print(n,end="")
+        print("")
 printNumbers(5) 
 
         
